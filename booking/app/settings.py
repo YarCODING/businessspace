44fd301df6534e173a17096eed113150aa4ecbc9
Yaroslav <20011aric@gmail.com>
2026-06-13 18:11:49 +0300

    pyanywhere hsting setup 2

--- booking/app/settings.py
+++ booking/app/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'booking',
-        'USER': 'booking-admin',
-        'PASSWORD': 'adminlovesuchi123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
